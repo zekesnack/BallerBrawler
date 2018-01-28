0d00ac22794c7eeea87e39f86a8c8cf17ac5d56b
Dieter103 <grosswiler2@gmail.com>
2018-01-27 21:22:46 -0700

    adding menu

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class BulletController : MonoBehaviour {
 	private void OnCollisionEnter(Collision other) {
 		if (other.gameObject.CompareTag("Player")) {
+			var hit = other.gameObject;
 			other.transform.localScale += new Vector3(0.1F, 0.1F, 0.1F);
 			other.gameObject.GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().velocity * other.transform.localScale.magnitude);
+			var health = hit.GetComponent<Health>();
+			if (health  != null)
+			{
+				health.TakeDamage(10);
+			}
 		}
 		Destroy(gameObject);
 	}
