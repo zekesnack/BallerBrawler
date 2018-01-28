7860585eb0328c4549ad0a4a1c5260139aa6d4fa
Daniel Church <daniellchurch@fvcc.edu>
2018-01-27 17:00:45 -0700

    a

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public class PlayerController : NetworkBehaviour {
 	private int jumps = 0;
 
 	public int maxJumps = 2;
+
+	public GameObject child;
+
+	[SyncVar]
+	public Transform childtran;
 	
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -19,15 +24,30 @@ public class PlayerController : NetworkBehaviour {
 	}
 
 	void FixedUpdate () {
-		if (!isLocalPlayer)
+		if (!isLocalPlayer) {
+			child.transform.position = childtran.position;
+			child.transform.rotation = childtran.rotation;
+			child.transform.localScale = childtran.transform.localScale;
 			return;
-		
+		}
+
 		rb.velocity = new Vector3 (Input.GetAxis ("Horizontal") * 10, rb.velocity.y, rb.velocity.z);
 
 		if (Input.GetKeyDown("space") && jumps < maxJumps) {
 			rb.AddForce (0, 500, 0);
 			jumps++;
 		}
+		
+		// Get the direction between the shoulder and mouse (aka the target position)
+		var shoulderToMouseDir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+		shoulderToMouseDir.z = 0; // zero z axis since we are using 2d
+		// we normalize the new direction so you can make it the arm's length
+		// then we add it to the shoulder's position
+		child.transform.position = transform.position + (1 * shoulderToMouseDir.normalized);
+
+		child.transform.LookAt((child.transform.position - transform.position).normalized * 500);
+		childtran = child.transform;
 	}
 
 	private void OnCollisionEnter(Collision other) {
---
+++ Assets/Scripts/gunControls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class gunControls : MonoBehaviour {
+    // Point you want to have sword rotate around
+    public Transform Player;
+
+    // how far you want the sword to be from point
+    public float armLength = 1f;
+
+    private bool isPlayer;
+
+    void Start() {
+        // if the sword is child object, this is the transform of the character (or shoulder)
+        Player = transform.parent.transform;
+        isPlayer = Player.gameObject.GetComponent<NetworkBehaviour>().isLocalPlayer;
+    }
+
+    void Update() {
+    }
+}
